6397e598b2079b9df834bdb0ca7dca5c13d6541f
Yonatan Rojas <yonatanrojaszero@gmail.com>
2022-05-05 21:41:20 -0300

    cambios en contacto

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { icon } from '@fortawesome/fontawesome-svg-core';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-contacto',
@@ -6,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacto.component.scss']
 })
 export class ContactoComponent implements OnInit {
-
+  faPhone = faPhone;
+  icon = icon;
   constructor() { }
 
   ngOnInit(): void {
